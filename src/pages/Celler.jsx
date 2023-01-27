d4064f06ceafdd24af2b9304add81e9af4f86bd7
jordisanchezntq <jordi.sanchez@gfk.com>
2023-01-27 02:20:56 +0100

    celler

--- src/pages/Celler.jsx
+++ src/pages/Celler.jsx
@@ -6,28 +6,27 @@ import Footer from '../components/Footer'
 
 function Celler() {
   return (
-    <section className='bg-dark text-light'>
-      <div className='h-screen'>
-        <img src={CopesVi} alt="Copes de vi" className='block'/>
-
+    <section className='text-light bg-dark'>
+      <div className='h-[500px] md:h-screen py-5'>
+        <img src={CopesVi} alt="Copes de vi" className='block mx-auto w-screen'/>
         <SubHeading>
           El Celler Cusó
         </SubHeading>
+        <hr className='border-b-8' />
       </div>
 
-      <div className='w-full lg:max-w-screen-xl mx-auto'>
-         <div className='w-full lg:max-w-screen-lg mx-auto h-auto py-8 lg:py-12 flex flex-col md:flex-row items-center'>
-            <div className='py-8 lg:py-3 md:w-1/2'>
-              <SubHeading>Essències locals</SubHeading>
-              <p className='px-10 text-lg'>
-              Els vins més bons són capaços d'expressar un paisatge i transmetre la fascinació de la seva personalitat única. Hem volgut crear l'acompanyant perfecte per a tots els nostres pats, creant una versió en negre com en blanc.
-              </p>
-            </div>
-            <img src={ArnauVi} alt="imatge Arnau Vi" className='block md:w-1/2 w-full shadow-xl order-2 md:order-none'/>
-        </div>
-    </div>
+      <div className='w-full lg:max-w-screen-xl mx-auto py-8 lg:py-10 mb-40 flex flex-col md:flex-row items-center'>
+          <img src={ArnauVi} alt="imatge forn llenya" className='block md:w-1/4 order-2 md:order-none -24 rounded-r-lg shadow-lg shadow-light'
+             />
+          <div className='p-0 w-full'>
+              <SubHeading>L'essència familiar al teu paladar</SubHeading>
+          <p className='px-10 py-4'>
+              Les vinyes situades a Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa, inventore! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores reiciendis ea voluptatum, modi non illum veniam amet culpa animi fugit!
+          </p>
+          </div>
+      </div>
 
-    <div className='bg-light'>
+    <div className='bg-gradient-to-b from-light/75 to-light'>
       <Footer />
     </div>
 
